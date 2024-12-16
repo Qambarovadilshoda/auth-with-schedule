92e8ff3d1eba7a88ccb8fbbf90aff06cf58ea296
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-16 15:00:28 +0500

    Added error handler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,12 @@
 <?php
 
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
+use App\Http\Middleware\SetLocaleMiddleware;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,8 +16,17 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'setLocale' => SetLocaleMiddleware::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if (request()->is('api/*') && $e->getPrevious() instanceof ModelNotFoundException) {
+                $model = Str::afterLast($e->getPrevious()->getModel(), '\\');
+                return response()->json(['message' => $model . ' not found'], 404);
+            }
+
+            return response()->json(['message' => __('errors.404')], 404);
+        });
     })->create();
