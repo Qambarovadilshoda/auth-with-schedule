fc70361ccf646c2b2c18c3147e6299b2f2d472db
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-16 12:53:17 +0500

    I added routes for authentication

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function (){
+    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'getUser']);
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/email-verify', [AuthController::class, 'emailVerify'])->name('verify');
