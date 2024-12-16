957a0827216a6ccf613929671418d615c5a58f39
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-16 13:12:29 +0500

    Message was written to blade

--- resources/views/email/send.blade.php
+++ resources/views/email/send.blade.php
@@ -1,3 +1,4 @@
 <div>
-    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
+    <h1>Hello, {{$name}}</h1>
+    <a href="{{$link}}"><h4>Email verification</h4></a>
 </div>
