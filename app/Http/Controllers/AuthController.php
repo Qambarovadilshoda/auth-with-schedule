2a284b173f299a97b5d57de7eadf27dd60c1c206
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-16 14:19:12 +0500

    Was written login and emailVerify methods

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\UserResource;
 use App\Models\User;
 use App\Jobs\SendEmailJob;
 use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
+use App\Http\Resources\UserResource;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\RegisterRequest;
 
 class AuthController extends Controller
@@ -24,12 +25,27 @@ class AuthController extends Controller
         return $this->success(new UserResource($user), 'User created successfully', 201);
     }
     public function login(LoginRequest $request){
-
+        $user = User::where('email', $request->email)->firstOrFail();
+        if($user->email_verified_at == null){
+            return $this->error('Email not verified', 403);
+        }
+        if(!Hash::check($request->password, $user->password)){
+            return $this->error('Password is incorrect');
+        }
+        
     }
     public function getUser(Request $request){
 
     }
     public function logout(Request $request){
+
+    }
+    public function emailVerify(Request $request){
+        $user = User::where('verification_token', $request->token)->firstOrFail();
+        $user->email_verified_at = now();
+        $user->save();
+
+        return $this->success([], 'Email verified successfully');
 
     }
 }
