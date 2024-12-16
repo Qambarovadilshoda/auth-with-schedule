a04fc7f6dff55eb4a3bf8505deccc523d0c80360
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-16 15:14:49 +0500

    Added validation files for languages ru and uz

---
+++ lang/ru/validation.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'required' => ':attribute требуется.',
+    'regex' => ':attribute формат неверный.',
+    'unique' => ':attribute уже существует.',
+    'exists' => 'Выбрано :attribute неверный.',
+    'min' => [
+        'numeric' => ':attribute по меньшей мере :min должен состоять из символа.',
+    ]
+];
---
+++ lang/uz/validation.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'required' => ':attribute talab qilinadi.',
+    'regex' => ':attribute formati xato.',
+    'unique' => ':attribute allaqachon mavjud.',
+    'exists' => 'Tanlangan :attribute xato.',
+    'min' => [
+        'numeric' => ':attribute kamida :min belgidan iborat bo\'lishi kerak.',
+    ]
+];
