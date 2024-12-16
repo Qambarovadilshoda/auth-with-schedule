32cf72c68ef93b00af968be8d2fa54c650139834
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-16 15:01:07 +0500

    Updated

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,14 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function (){
-    Route::get('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', [AuthController::class, 'getUser']);
-});
+Route::middleware('setLocale')->group(function(){
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/email-verify', [AuthController::class, 'emailVerify'])->name('verify');
+    Route::middleware('auth:sanctum')->group(function (){
+        Route::get('/logout', [AuthController::class, 'logout']);
+        Route::get('/user', [AuthController::class, 'getUser']);
+    });
+
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/email-verify', [AuthController::class, 'emailVerify'])->name('verify');
+});
